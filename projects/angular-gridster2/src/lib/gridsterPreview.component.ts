bbd35c0c8c9c1e2388fefdf23343d14c49cbcfc2
ketan-trigyn <86778996+ketan-trigyn@users.noreply.github.com>
2021-10-05 17:17:34 +0530

    first commit

---
+++ projects/angular-gridster2/src/lib/gridsterPreview.component.ts
@@ -0,0 +1,40 @@
+import {Component, ElementRef, EventEmitter, Inject, Input, OnDestroy, OnInit, Renderer2, ViewEncapsulation} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {GridsterRenderer} from './gridsterRenderer.service';
+import {GridsterItem} from './gridsterItem.interface';
+
+@Component({
+  selector: 'gridster-preview',
+  template: '',
+  styleUrls: ['./gridsterPreview.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class GridsterPreviewComponent implements OnInit, OnDestroy {
+  @Input() previewStyle$: EventEmitter<GridsterItem>;
+  @Input() gridRenderer: GridsterRenderer;
+  private el: HTMLElement;
+  private sub: Subscription;
+
+  constructor(@Inject(ElementRef) el: ElementRef, @Inject(Renderer2) private renderer: Renderer2) {
+    this.el = el.nativeElement;
+  }
+
+  ngOnInit(): void {
+    this.sub = this.previewStyle$.subscribe(options => this.previewStyle(options));
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+    // @ts-ignore
+    delete this.el;
+  }
+
+  private previewStyle(item: GridsterItem): void {
+    if (item) {
+      this.renderer.setStyle(this.el, 'display', 'block');
+      this.gridRenderer.updateItem(this.el, item, this.renderer);
+    } else {
+      this.renderer.setStyle(this.el, 'display', '');
+    }
+  }
+}
